8eb596de7771348372a339483725d17522e26564
cometRush <136585160+cometRush@users.noreply.github.com>
2026-07-07 13:00:28 +0700

    Update Product.h
    Thêm getter và setter để shopmanager thấy và cập nhật dữ liệu

--- Product.h
+++ Product.h
@@ -1,15 +1,32 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
 #include <string>
+#include <iostream>
 
-class Product {
+class Product{
 	private:
 		std::string productTypes, productName;
 		int quantities, id;
 		double price;
 	public:
-    Product(std::string name, std::string t, int q, int id, double p): productName(name), productTypes(t), quantities(q), id(id), price(p){}
-    virtual displayProduct() = 0
-    virtual ~Product() {}
+		Product(std::string name, std::string t, int q, int id, double p): productName(name), productTypes(t), quantities(q), id(id), price(p){}
+		
+		// Getter & Setter for Order and ShopManager to update the item
+		int quantitiesGet() const {return quantities;}
+		int idGet() const {return id;}
+		double priceGet() const {return price;}
+		std::string typeGet() const {return productTypes;}
+		std::string nameGet() const {return productName;}
+		
+		void quantitiesSet(int newQuantities) {quantities = newQuantities;}
+		void priceSet(double newPrice) {price = newPrice;}
+		// No need setter for product name, type, or id
+		
+		
+		void displayProduct() const {
+			std::cout << "Item: " << productName << " " << id << ": $" << price << " ( " << quantities << " in stock. )" << std::endl;
+		}
+		
 };
+
 #endif
