75259d296981d58fe92506fc470978b3056fb943
cometRush <136585160+cometRush@users.noreply.github.com>
2026-07-07 14:17:26 +0700

    Update ShopManager.h
    VER 1 HELP

--- ShopManager.h
+++ ShopManager.h
@@ -1,13 +1,38 @@
 #ifndef SHOPMANAGER_H
 #define SHOPMANAGER_H
 #include "Product.h"
+#include "Order.h"
 #include <vector>
+#include <iostream>
 #include <string>
+
 
 class ShopManager{
 	private:
-		std::vector inventory;
+		int uniqueId;
+		std::vector<Product> inventory;
 	public:
-		ShopManager(std::vector inv): inventory(inv){}
+		ShopManager() {} //DEFAULT CONSTRUCTION, DO NOT DELETE
+		ShopManager(std::vector<Product> inv): inventory(inv){uniqueId = 1;}
+		
+		// ai do fix phan nay di
+		void addProductViaAdmin(std::string name,std::string type,double price, int quantities) {
+			std::cout << "\n--- ADD NEW PRODUCT ---\n";
+			std::cin.ignore();
+		
+			std::cout << "Enter Product Name: ";
+			getline(std::cin, name);
+			
+			std::cout << "Enter Product Category: ";
+			getline(std::cin, type);
+		
+			std::cout << "Enter Price ($): ";
+			std::cin >> price;
+		
+			std::cout << "Enter Initial Stock Quantity: ";
+			std::cin >> quantities;
+		}
+		int nextId() {uniqueId++;}
 };
+
 #endif
