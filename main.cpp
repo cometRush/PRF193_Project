c83f83ae0769ed2124401ab5f861921baedaa3b8
cometRush <136585160+cometRush@users.noreply.github.com>
2026-07-07 14:18:06 +0700

    Add files via upload
    still need fixing

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "ShopManager.h"
+#include "Product.h"
+#include "Order.h"
+#include <string>
+#include <fstream>
+using namespace std;
+
+int id;
+string name, type;
+double price;
+int quantities;
+
+int main() {
+	cout << "--- \e[1;91m ADMIN PANEL \e[0m ---\n" << endl;
+    int choice;
+    ShopManager shop;
+    
+	cout << "1. Add Product to Spreadsheet\n";
+	cout << "2. Exit\n";
+	cin >> choice;
+	
+	switch (choice) {
+		case 1:{
+			
+			//need fixing
+			shop.addProductViaAdmin(name, type,price,quantities);
+			id = shop.nextId();
+			ofstream file("inventory.csv", ios::app);
+			
+			if (file.is_open()) {
+				file << shop.nextId() << "," << name << "," << price << "," << quantities << "\n";
+				file.close(); 
+				cout << "\n[Success] Product successfully saved to inventory.csv!\n";
+			} 
+			else {
+				cerr << "\n[Error] Could not open the spreadsheet file.\n";
+			}
+			break;
+		}
+			
+		default:
+			cout << "[Error] Incorrect syntax.\n";
+	}
+    
+	return 0;
+}
